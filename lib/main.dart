a0d31a283d2ba3d8250d91bdb80d1061b514dd28
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2020-07-21 23:12:26 -0500

    Add logic for Magic 8 Ball application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Magic8Ball(),
+      ),
+    ),
+  );
+}
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballAnswer = 1;
+
+  void changeBallImage() {
+    setState(() {
+      ballAnswer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeBallImage();
+              },
+              child: Image.asset('images/ball$ballAnswer.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
